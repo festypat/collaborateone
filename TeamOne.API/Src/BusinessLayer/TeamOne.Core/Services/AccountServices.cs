dc8add0f69e9c90215431f1474b3ca3b2f8b2d3c
festypat <festypat9@gmail.com>
2025-03-24 00:08:07 +0100

    Created account services

---
+++ TeamOne.API/Src/BusinessLayer/TeamOne.Core/Services/AccountServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOne.Core.Services;
+
+public class AccountServices : IAccountServices
+{
+    public AccountServices()
+    {
+
+    }
+
+    public string BalanceEnquiry(string accountId, string accountName)
+    {
+        return string.Empty;
+    }
+}
---
+++ TeamOne.API/Src/BusinessLayer/TeamOne.Core/Services/IAccountServices.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamOne.Core.Services
+{
+    public interface IAccountServices
+    {
+        string BalanceEnquiry(string accountId, string accountName);
+    }
+}
