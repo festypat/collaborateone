e16bd68c3313d6ac0ee66480f71e9d56d7b69c07
festypat <festypat9@gmail.com>
2025-03-24 09:32:39 +0100

    Modified service

--- TeamOne.API/Src/BusinessLayer/TeamOne.Core/Services/IAccountServices.cs
+++ TeamOne.API/Src/BusinessLayer/TeamOne.Core/Services/IAccountServices.cs
@@ -5,9 +5,6 @@ public interface IAccountServices
     string BalanceEnquiry(string accountId, string accountName);
     string NameEnquiry(string accountId, string accountName);
     string GetAccountDetails(string accountId, string accountName);
-<<<<<<< HEAD
     string GetCustomerDetails(string accountId, string accountName);
-=======
     string GetBvn(string accountId, string accountName);
->>>>>>> fdaa1468f2a01a9d4311fc6b241121bcdd5fd8b3
 }
