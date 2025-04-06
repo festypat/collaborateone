937be181348e3af6b215bf4f5bc5c0f21c033c15
Bii-peerless <abidemi@bepeerless.co>
2025-04-06 22:58:47 +0100

    Modified salary service

---
+++ TeamOne.API/Src/BusinessLayer/TeamOne.Core/Services/SalaryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOne.Core.Services;
+
+public class SalaryService
+{
+    public string PaySalary(string salary)
+    {
+        return salary;
+    }
+}
